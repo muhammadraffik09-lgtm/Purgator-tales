9608e6ebb94afe3e6087b8a1a0432937e869a7e0
Zidane Akbar <zidane2.3akbar@gmail.com>
2026-09-15 20:07:37 +0700

    Credits Menu

--- control.gd
+++ control.gd
@@ -2,9 +2,11 @@ extends Control
 
 @onready var pause_menu = $PauseMenu
 
+
 func _ready():
 	pause_menu.visible = false
 	get_tree().paused = false
+
 
 func _on_pause_button_pressed():
 	if get_tree().paused:
@@ -14,19 +16,27 @@ func _on_pause_button_pressed():
 		pause_menu.visible = true
 		get_tree().paused = true
 
+
 func _on_resume_button_pressed():
 	get_tree().paused = false
 	pause_menu.visible = false
 
-func _on_back_button_pressed():
-	$PauseMenu.visible = true
-	$PauseMenu/SettingsMenu.visible = false
+
+func _on_setting_button_pressed():
+	$PauseMenu.visible = false
+	$SettingsMenu.visible = true
+
+
+func _on_credits_button_pressed():
+	$PauseMenu.visible = false
+	$CreditsMenu.visible = true
 
 
 func _on_settings_menu_back_pressed():
 	$SettingsMenu.visible = false
 	$PauseMenu.visible = true
 
-func _on_setting_button_pressed():
-	$PauseMenu.visible = false
-	$SettingsMenu.visible = true
+
+func _on_credits_menu_back_pressed():
+	$CreditsMenu.visible = false
+	$PauseMenu.visible = true
--- settings_menu.gd
+++ settings_menu.gd
@@ -2,8 +2,58 @@ extends Control
 
 signal back_pressed
 
+@onready var pause_menu = get_parent().get_node("PauseMenu")
+
+const SETTINGS_FILE := "user://settings.cfg"
+const SETTINGS_SECTION := "UI"
+const DEFAULT_TRANSPARENCY := 20.0
+
 func _ready():
 	process_mode = Node.PROCESS_MODE_ALWAYS
+
+	var config = ConfigFile.new()
+	var error = config.load(SETTINGS_FILE)
+
+	var transparency := DEFAULT_TRANSPARENCY
+
+	if error == OK:
+		transparency = config.get_value(
+			SETTINGS_SECTION,
+			"transparency",
+			DEFAULT_TRANSPARENCY
+		)
+
+	$Panel/SettingsScroll/SettingsList/UITransparency/TransparencyRow/UITransparencySlider.value = transparency
+
+	_apply_transparency(transparency)
+
+
+func _on_ui_transparency_slider_value_changed(value: float):
+	_apply_transparency(value)
+	_save_transparency(value)
+
+
+func _apply_transparency(value: float):
+	var alpha = 1.0 - (value / 100.0)
+
+	$Panel.modulate.a = alpha
+	pause_menu.modulate.a = alpha
+
+	$Panel/SettingsScroll/SettingsList/UITransparency/TransparencyRow/UITransparencyValue.text = str(int(value)) + "%"
+
+func _save_transparency(value: float):
+	var config = ConfigFile.new()
+
+	config.load(SETTINGS_FILE)
+
+	config.set_value(
+		SETTINGS_SECTION,
+		"transparency",
+		value
+	)
+
+	config.save(SETTINGS_FILE)
+
 
 func _on_back_button_pressed():
 	back_pressed.emit()
