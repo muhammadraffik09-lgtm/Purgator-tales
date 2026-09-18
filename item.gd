5be6e52ca7b9ab44fe1d17f723d6234c554296a7
muhammadraffik09-lgtm <muhammadraffik09@gmail.com>
2026-09-18 14:32:25 +0700

    tes

--- item.gd
+++
@@ -1,9 +0,0 @@
-extends Area2D
-
-func _on_body_entered(body: Node2D) -> void:
-	# Memastikan bahwa yang menyentuh item adalah Player
-	if body.is_in_group("player"):
-		# Tambahkan logika permainan di sini (misal: tambah skor/HP)
-		
-		# Hapus item dari scene/layar
-		queue_free()
